566db338b21581ff1a05b41c3369d163c2a1a972
yuvithakur007 <01yuvisingh@gmail.com>
2023-10-06 07:31:20 +0530

    insert one

---
+++ insertOne.java
@@ -0,0 +1,54 @@
+public class insertOne {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public insertOne() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        insertOne first = new insertOne();
+        first.insertFirst(1);
+        first.insertFirst(3);
+        first.insertFirst(5);
+        first.insertFirst(10);
+        first.display();
+    }
+}
