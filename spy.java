a6cd0f131d9e814f72a343b46cdb8b480c0166b8
yuvithakur007 <01yuvisingh@gmail.com>
2023-10-13 17:54:17 +0530

    spy

---
+++ spy.java
@@ -0,0 +1,23 @@
+public class spy {
+    public static void main(String[] args){
+        // find spy number or nor
+        // whose sum and product or digits are same
+        int n = 1124;
+        int sum = 0;
+        int product = 1;
+        while(n>0){
+            int rem = n%10;
+            sum = sum + rem;
+            product = product * rem;
+            n = n/10;
+        }
+        if(sum == product){
+            System.out.println("Spy Number");
+        }
+        else{
+            System.out.println("Not a Spy Number");
+        }
+        }
+        
+    }
+}   
