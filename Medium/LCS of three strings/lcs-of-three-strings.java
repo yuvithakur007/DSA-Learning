8004c42149cdf9c757c67d35e5fab01f02cd6db5
Yuvraj Singh <01yuvisingh@gmail.com>
2024-01-30 00:48:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/LCS of three strings/lcs-of-three-strings.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException 
+    { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int n1 = Integer.parseInt(input_line[0]);
+            int n2 = Integer.parseInt(input_line[1]);
+            int n3 = Integer.parseInt(input_line[2]);
+            input_line = read.readLine().trim().split("\\s+");
+            String A = input_line[0];
+            String B = input_line[1];
+            String C = input_line[2];
+            Solution obj = new Solution();
+            System.out.println(obj.LCSof3(A, B, C, n1, n2, n3));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    int LCSof3(String a, String b, String c, int m, int n, int o) {
+        int[][][] dp = new int[m + 1][n + 1][o + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= o; k++) {
+                    if ((a.charAt(i - 1) == b.charAt(j - 1)) && (b.charAt(j - 1) == c.charAt(k - 1))) {
+                        dp[i][j][k] = 1 + dp[i - 1][j - 1][k - 1];
+                    } else {
+                        dp[i][j][k] = Math.max(Math.max(dp[i - 1][j][k], dp[i][j - 1][k]), dp[i][j][k - 1]);
+                    }
+                }
+            }
+        }
+        return dp[m][n][o];
+    }
+}
